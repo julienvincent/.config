6052fd915ad27e86aca88c47c9a520e01f0db5a3
Julien Vincent <m@julienvincent.io>
2024-03-10 11:13:09 +0200

    Add global clojure config

---
+++ clojure/user.clj
@@ -0,0 +1,42 @@
+(in-ns 'user)
+
+(require '[clj-reload.core :as reload])
+(require '[malli.dev :as dev])
+(require '[clojure.string :as str])
+(require '[clojure.java.io :as io])
+(require '[clojure.pprint :as pprint])
+
+(def m2-dir
+  (str (System/getenv "HOME") "/.m2"))
+
+(def classpath
+  (into []
+        (comp
+         (filter (fn [path]
+                   (not (str/starts-with? path m2-dir))))
+         (filter (fn [path]
+                   (let [file (io/file path)]
+                     (and (.exists file)
+                          (.isDirectory file)))))
+         (filter (fn [path]
+                   (not (some #{path} #{"dev" "local"})))))
+
+        (str/split (System/getProperty "java.class.path") #":")))
+
+(reload/init {:dirs classpath})
+
+(add-tap
+ (bound-fn*
+  (fn [data]
+    (binding [*out* (java.io.PrintWriter. (java.io.OutputStreamWriter. System/out))]
+      (pprint/pprint data)))))
+
+(defn instrument! []
+  #_(binding [*out* (java.io.PrintWriter. (java.io.OutputStreamWriter. System/out))]
+      (dev/start!)))
+
+(defn reload-namespaces []
+  (reload/reload)
+  (instrument!))
+
+(instrument!)
