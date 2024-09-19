f818e46f34642ce904d3db55e900310de095a7ab
Julien Vincent <m@julienvincent.io>
2024-09-19 21:51:58 +0100

    Ensure ~/.local/bin takes precedence over homebrew

--- fish/conf.d/path.fish
+++ fish/conf.d/path.fish
@@ -1,7 +1,7 @@
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
 fish_add_path $HOME/.local/google-cloud-sdk/bin
-fish_add_path $HOME/.local/bin
+fish_add_path --move $HOME/.local/bin
 
 set -gx PNPM_HOME "$HOME/Library/pnpm"
 if not string match -q -- $PNPM_HOME $PATH
