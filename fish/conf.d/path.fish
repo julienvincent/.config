8e3d6889040578f34d8878bc834cbfa1ec8640a4
Julien Vincent <m@julienvincent.io>
2024-09-19 21:51:58 +0100

    Add gnubin install over default mac installs

--- fish/conf.d/path.fish
+++ fish/conf.d/path.fish
@@ -3,6 +3,8 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 fish_add_path $HOME/.local/google-cloud-sdk/bin
 fish_add_path --move $HOME/.local/bin
 
+fish_add_path --move "/opt/homebrew/opt/make/libexec/gnubin"
+
 set -gx PNPM_HOME "$HOME/Library/pnpm"
 if not string match -q -- $PNPM_HOME $PATH
     set -gx PATH "$PNPM_HOME" $PATH
