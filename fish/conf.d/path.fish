9d9ceaaed3b29f382a43ad85e6a96c6607dee8b3
Julien Vincent <m@julienvincent.io>
2024-09-19 21:51:58 +0100

    Configure PNPM_HOME env var

--- fish/conf.d/path.fish
+++ fish/conf.d/path.fish
@@ -2,3 +2,8 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 
 fish_add_path $HOME/.local/google-cloud-sdk/bin
 fish_add_path $HOME/.local/bin
+
+set -gx PNPM_HOME "$HOME/Library/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
