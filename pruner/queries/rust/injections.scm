ac32f867a84fc838a3860b9cf0b12b89193e90bd
Julien Vincent <m@julienvincent.io>
2024-09-19 21:51:58 +0100

    Update pruner config for version 1.0.0-alpha.7

--- pruner/queries/rust/injections.scm
+++ pruner/queries/rust/injections.scm
@@ -1,3 +1,4 @@
 ((string_content) @injection.content
   (#match? @injection.content "^(SET|TRUNCATE|SELECT|CREATE|DELETE|ALTER|UPDATE|DROP|INSERT|WITH)")
+  (#escape! @injection.content "\"")
   (#set! injection.language "sql"))
