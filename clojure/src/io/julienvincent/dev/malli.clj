1ab26f39924fc263f86cf1600569839bb8f22b9b
Julien Vincent <m@julienvincent.io>
2026-01-27 16:45:18 +0000

    Handle additional malli instrumentation errors

--- clojure/src/io/julienvincent/dev/malli.clj
+++ clojure/src/io/julienvincent/dev/malli.clj
@@ -2,6 +2,34 @@
   (:require
    [malli.dev :as dev]))
 
+(defn- report-fn [report key data]
+  (when (= :malli.core/register-function-schema key)
+    (throw (ex-info (str "Failed to register function schema for "
+                         (:ns data) "/" (:name data))
+                    {:ns (:ns data)
+                     :name (:name data)})))
+
+  (when (:log report)
+    (tap> {:type key
+           :data data}))
+
+  (when (:throw report)
+    (case key
+      :malli.core/invalid-schema
+      (throw (ex-info "Invalid malli schema" data))
+
+      :malli.core/invalid-entry
+      (throw (ex-info "Invalid entry" data))
+
+      (throw (ex-info (str "Function "
+                           (:fn-name data)
+                           " schema not satisfied - "
+                           key)
+                      (cond-> {:input (:args data)
+                               :type key
+                               :fn (:fn-name data)}
+                        (:value data) (assoc :output (:value data))))))))
+
 (defn setup-malli-instrumentation [config]
   (let [{:keys [enable report]
          :or {enable true
@@ -12,34 +40,8 @@
                  #{report})]
 
     (when enable
-      (try
-        (dev/start!
-         {:report
-          (fn [key data]
-            (when (= :malli.core/register-function-schema key)
-              (throw (ex-info (str "Failed to register function schema for "
-                                   (:ns data) "/" (:name data))
-                              {:ns (:ns data)
-                               :name (:name data)})))
-
-            (when (:log report)
-              (tap> {:type key
-                     :data data}))
-
-            (when (:throw report)
-              (case key
-                :malli.core/invalid-schema
-                (throw (ex-info "Invalid malli schema" data))
-
-                (throw (ex-info (str "Function "
-                                     (:fn-name data)
-                                     " schema not satisfied - "
-                                     key)
-                                (cond-> {:input (:args data)
-                                         :type key
-                                         :fn (:fn-name data)}
-                                  (:value data) (assoc :output (:value data))))))))})
-
-        (catch Exception ex
-          (tap> "Failed to initialize malli instrumentation")
-          (tap> ex))))))
+      (try (dev/start! {:report (fn [key data]
+                                  (report-fn report key data))})
+           (catch Exception ex
+             (tap> "Failed to initialize malli instrumentation")
+             (tap> ex))))))
