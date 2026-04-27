01df94344c6ded80f7b119d50c894e3a36c5fa96
Julien Vincent <m@julienvincent.io>
2026-04-27 11:04:54 +0100

    Expand backup prune to cover a wider period

--- restic/lunos/prune.sh
+++ restic/lunos/prune.sh
@@ -6,8 +6,11 @@ SCRIPT_DIR="$(cd -P -- "$(dirname -- "$(readlink -f -- "${BASH_SOURCE[0]}")")" &
 source "$SCRIPT_DIR/auth.sh"
 
 restic forget \
+  --keep-hourly 48 \
   --keep-daily 7 \
-  --keep-weekly 4 \
-  --keep-monthly 6 \
+  --keep-weekly 8 \
+  --keep-monthly 12 \
+  --keep-yearly 2 \
+  --group-by=host \
   --prune \
   "$@"
